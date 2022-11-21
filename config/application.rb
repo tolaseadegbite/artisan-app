2ae8c54c2894f39c3ae5678a7bf9cdeb54119853
tolaseadegbite <tolasekelvin@gmail.com>
2022-11-21 19:15:06 +0100

    Modify navbar

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module ArtisanApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.active_storage.variant_processor = :vips
+    config.active_storage.variant_processor = :mini_magick
 
     # Configuration for the application, engines, and railties goes here.
     #
