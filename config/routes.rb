d51dfb21a8b60f0637dc2be7c3916b537a711815
tolaseadegbite <tolasekelvin@gmail.com>
2022-12-18 10:16:19 +0100

    Add Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ end
   resources :notifications, only: [:index]
   resources :announcements, only: [:index]
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
-  root to: 'profiles#index'
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # post "profile/message" => "profiles#email_profile", as: :email_profile
